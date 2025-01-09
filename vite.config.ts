1f76460feac3611a14e1273498e3aa09a1c8bc55
cristiancazzaniga <cristian.cazzaniga@issgreppi.it>
2025-01-09 13:47:10 +0100

    sistemate alcune cose 2.0'

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default {
   plugins: [vue()],
+  build: {
+    target: 'esnext',  // Usa ESNext per supportare i moduli moderni
+    sourcemap: true,   // Abilita la generazione di sourcemap per il debugging
+  },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': '/src',
     }
-  }
-});
+  },
+}
