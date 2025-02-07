140205240294d27c163b06bfb64a9c88f6548c9f
cristiancazzaniga <cristian.cazzaniga@issgreppi.it>
2025-02-07 14:18:34 +0100

    aggiunta search bar da mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,64 @@
+
+<template>
+  <nav class="navbar">
+    <div class="navbar-left">
+      <img src="@/assets/logo.png" alt="Logo" class="logo" />
+      <h1>Evently</h1>
+    </div>
+    <div class="navbar-center">
+      <router-link v-if="isAuthenticated" :to="`/profilo/${logUserId}`" id="profilo">
+        <span class="username">
+          <img v-if="logProfilePicture" :src="logProfilePicture" alt="Foto Profilo" class="profile-picture" />
+          <span class="nomeProfilo">{{ logUserName }}</span>
+        </span>
+      </router-link>
+      <router-link v-if="!isAuthenticated" to="/login" id="home">ACCEDI</router-link>
+      <router-link to="/" id="mappa">MAPPA</router-link>
+      <router-link v-if="!isAuthenticated" to="/login" id="profilo" class="profilo-link disabled" @click.prevent>
+        PROFILO
+      </router-link>
+      <router-link to="/chiSiamo" id="chiSiamo">CHI SIAMO</router-link>
+    </div>
+    <div class="navbar-right">
+      <div v-if="showSearchBar" class="search-container">
+        <input type="text" id="search" placeholder="Cerca" class="search-bar" v-model="searchQuery"
+          @input="handleSearchInput" autocomplete="off" />
+        <div id="suggestions" class="suggestions">
+          <div v-for="(city, index) in suggestions" :key="index" class="suggestion"
+            @click="handleSuggestionClick(city)">
+            {{ city.name }}
+          </div>
+        </div>
+      </div>
+      <div class="settings-container" 
+      @mouseenter="!isMobile && (isSettingsMenuVisible = true)"
+      @mouseleave="!isMobile && (isSettingsMenuVisible = false)"
+      @click="isMobile && (isSettingsMenuVisible = !isSettingsMenuVisible)">
+      <img src="@/assets/imp.png" alt="Impostazioni" class="settings-icon" />
+    
+      <div class="settings-menu" v-if="isSettingsMenuVisible">
+        <ul>
+          <li @click="showAlert">Gestione eventi <span>></span></li>
+          <li @click="showAlert">Lingua <span>></span></li>
+          <li @click="showAlert">Centro sulla privacy <span>></span></li>
+          <li @click="showAlert">Archivio Post <span>></span></li>
+          <li @click="showAlert">Autorizzazioni del sito web <span>></span></li>
+          <li @click="showAlert">Utenti bloccati <span>></span></li>
+          <li @click="showAlert">Elimina account <span>></span></li>
+          <li v-if="isAuthenticated" @click="handleLogout">Log-out <span>></span></li>
+          <div v-if="!isAuthenticated">
+            <router-link to="/login">
+              <li>Log in <span>></span></li>
+            </router-link>
+          </div>
+        </ul>
+      </div>
+    </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped src="@/styles/navbar.css"></style>
 <script setup>
 import { computed, ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router'; // Importa useRoute
@@ -77,64 +138,3 @@ function showAlert() {
 }
 
 </script>
-
-<template>
-  <nav class="navbar">
-    <div class="navbar-left">
-      <img src="@/assets/logo.png" alt="Logo" class="logo" />
-      <h1>Evently</h1>
-    </div>
-    <div class="navbar-center">
-      <router-link v-if="isAuthenticated" :to="`/profilo/${logUserId}`" id="profilo">
-        <span class="username">
-          <img v-if="logProfilePicture" :src="logProfilePicture" alt="Foto Profilo" class="profile-picture" />
-          <span class="nomeProfilo">{{ logUserName }}</span>
-        </span>
-      </router-link>
-      <router-link v-if="!isAuthenticated" to="/login" id="home">ACCEDI</router-link>
-      <router-link to="/" id="mappa">MAPPA</router-link>
-      <router-link v-if="!isAuthenticated" to="/login" id="profilo" class="profilo-link disabled" @click.prevent>
-        PROFILO
-      </router-link>
-      <router-link to="/chiSiamo" id="chiSiamo">CHI SIAMO</router-link>
-    </div>
-    <div class="navbar-right">
-      <div v-if="showSearchBar" class="search-container">
-        <input type="text" id="search" placeholder="Cerca" class="search-bar" v-model="searchQuery"
-          @input="handleSearchInput" autocomplete="off" />
-        <div id="suggestions" class="suggestions">
-          <div v-for="(city, index) in suggestions" :key="index" class="suggestion"
-            @click="handleSuggestionClick(city)">
-            {{ city.name }}
-          </div>
-        </div>
-      </div>
-      <div class="settings-container" 
-      @mouseenter="!isMobile && (isSettingsMenuVisible = true)"
-      @mouseleave="!isMobile && (isSettingsMenuVisible = false)"
-      @click="isMobile && (isSettingsMenuVisible = !isSettingsMenuVisible)">
-      <img src="@/assets/imp.png" alt="Impostazioni" class="settings-icon" />
-    
-      <div class="settings-menu" v-if="isSettingsMenuVisible">
-        <ul>
-          <li @click="showAlert">Gestione eventi <span>></span></li>
-          <li @click="showAlert">Lingua <span>></span></li>
-          <li @click="showAlert">Centro sulla privacy <span>></span></li>
-          <li @click="showAlert">Archivio Post <span>></span></li>
-          <li @click="showAlert">Autorizzazioni del sito web <span>></span></li>
-          <li @click="showAlert">Utenti bloccati <span>></span></li>
-          <li @click="showAlert">Elimina account <span>></span></li>
-          <li v-if="isAuthenticated" @click="handleLogout">Log-out <span>></span></li>
-          <div v-if="!isAuthenticated">
-            <router-link to="/login">
-              <li>Log in <span>></span></li>
-            </router-link>
-          </div>
-        </ul>
-      </div>
-    </div>
-    </div>
-  </nav>
-</template>
-
-<style scoped src="@/styles/navbar.css"></style>
